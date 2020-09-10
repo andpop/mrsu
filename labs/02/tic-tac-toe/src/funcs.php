62cd0b35a5f91b38b6fad8cb56a4f30afaf6554c
Andrey Popov <andpop@mail.ru>
2020-09-10 16:21:54 +0300

    Change task02

---
+++ labs/02/tic-tac-toe/src/funcs.php
@@ -0,0 +1,19 @@
+/* View */
+drawBoard(board)
+
+/* Controller */
+Andpop\Tic-tac-toe\Controller
+startGame()  
+inputPlayerLetter()
+whoGoesFirst()
+
+/* Model */
+makeMove(board, letter, move)
+isWinner(board, letter)
+getBoardCopy(board)
+isSpaceFree(board, move)
+getPlayerMove(board)
+chooseRandomMoveFromList(board, movesList)
+getComputerMove(board, computerLetter)
+isBoardFull(board)
+
