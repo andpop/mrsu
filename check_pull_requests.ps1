48ecb9dadaf8d78e72ac95c6a9f7bc715a535be0
Andrey Popov <andpop@mail.ru>
2020-10-25 22:32:56 +0300

    Add lecture_06

--- check_pull_requests.ps1
+++ check_pull_requests.ps1
@@ -5,7 +5,9 @@ $headers.Add("Authorization", "Basic andpop-mrsu PAT-here!!!")
 
 Get-ChildItem $studentDir -Attributes Directory | 
     ForEach-Object { 
+        Out-Host -InputObject $_.Name
         $response = Invoke-RestMethod -Uri "${teacherRepo}$($_.Name)/pulls" -Headers $headers
-        $obj = ($response | Select-Object -Property @{label='repo';expression={$_.head.repo.name}}, number, title, created_at)
-        $obj
+        $obj = ($response | 
+            Select-Object -Property @{label='repo'; expression={$_.head.repo.name}}, @{label='student_repo'; expression={$_.head.repo.clone_url}}, number, title, created_at)
+        $obj | Format-List *
     }
