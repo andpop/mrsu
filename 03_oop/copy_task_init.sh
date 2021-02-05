f9411890a5b4a9e066822529d28f2117d532b4b9
Andrey Popov <andpop@mail.ru>
2021-02-05 09:10:17 +0300

    Add PHPUnit_example

--- 03_oop/copy_task_init.sh
+++ 03_oop/copy_task_init.sh
@@ -7,22 +7,23 @@ echo $pwd
 
 task_number=$1
 
-for student in $(cat "${pwd}/students_1.txt"); do
+for student in $(cat "${pwd}/students-list.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
     student_dir="${BASE_DIR}/402_${student}"
 
     cd "${student_dir}"
-    git fetch teacher master
-    git merge teacher/master -m "Pull from teacher"
+    # git fetch teacher master
+    # git merge teacher/master -m "Pull from teacher"
 
     # cp "${pwd}/tasks/README.md" "${student_dir}/"
-    cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
-    cp "${pwd}/tasks/PHP_instruction.md" "${student_dir}/"
-    cp "${pwd}/tasks/task02_1.md" "${student_dir}/task02.md"
+    # cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
+    # cp "${pwd}/tasks/PHP_instruction.md" "${student_dir}/"
+    # cp "${pwd}/tasks/task02_1.md" "${student_dir}/task02.md"
+    cp -R "${pwd}/tasks/PHPUnit_example" "${student_dir}/"
 
     git add -A
-    git commit -m "Add task02"
+    git commit -m "Add PHPUnit_example"
     git push teacher master
     cd "${pwd}"
 done
