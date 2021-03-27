94245921a0798907cfbf868fb6d9e6377265f3fd
Andrey Popov <andpop@mail.ru>
2021-03-27 05:31:40 +0300

    Change task06

--- 03_oop/copy_task_init.sh
+++ 03_oop/copy_task_init.sh
@@ -7,7 +7,7 @@ echo $pwd
 
 task_number=$1
 
-for student in $(cat "${pwd}/students-list.txt"); do
+for student in $(cat "${pwd}/students_1.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
     student_dir="${BASE_DIR}/402_${student}"
@@ -19,11 +19,11 @@ for student in $(cat "${pwd}/students-list.txt"); do
     # cp "${pwd}/tasks/README.md" "${student_dir}/"
     # cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
     # cp "${pwd}/tasks/PHP_instruction.md" "${student_dir}/"
-    cp "${pwd}/tasks/task05.md" "${student_dir}/task05.md"
+    cp "${pwd}/tasks/task06_1.md" "${student_dir}/task06.md"
     # cp -R "${pwd}/tasks/PHPUnit_example" "${student_dir}/"
 
     git add -A
-    git commit -m "Change task05"
+    git commit -m "Change task06"
     git push teacher master
     cd "${pwd}"
 done
