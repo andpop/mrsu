5d68193a3aa08d16d650a3fd9c216e3417c80fad
Andrey Popov <andpop@mail.ru>
2021-05-11 21:55:00 +0300

    Add task09 in DB

--- 02_db/copy_task_init.sh
+++ 02_db/copy_task_init.sh
@@ -8,7 +8,7 @@ echo $pwd
 task_number=$2
 
 # for student in $(cat "${pwd}/students-list.txt"); do
-for student in $(cat "${pwd}/list_1.txt"); do
+for student in $(cat "${pwd}/list5.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
     student_dir="${BASE_DIR}/303_${student}"
@@ -21,11 +21,11 @@ for student in $(cat "${pwd}/list_1.txt"); do
     # cp "${pwd}/tasks/task02.md" "${student_dir}/"
     # cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
     # cp "${pwd}/tasks/*.xls" "${student_dir}/"
-    cp "${pwd}/tasks/test_sql_1.md" "${student_dir}/test_sql.md"
+    cp "${pwd}/tasks/task09_5.md" "${student_dir}/task09.md"
     # cp "${pwd}/tasks/specification_01.md" "${student_dir}/specification.md"
 
     git add -A
-    git commit -m "Add test_sql"
+    git commit -m "Add task09"
     git push teacher master
     cd "${pwd}"
 done
