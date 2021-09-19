085bfc784f4bf1caf4d61ae2cce53758d7b2a473
Andrey Popov <andpop@mail.ru>
2021-09-20 00:02:45 +0300

    Add lecture2 notes

--- 02_db/copy_task_init.sh
+++ 02_db/copy_task_init.sh
@@ -18,8 +18,8 @@ for student in $(cat "${pwd}/students-list.txt"); do
     git merge teacher/master -m "Merging master from teacher repo"
 
     # cp "${pwd}/tasks/README.md" "${student_dir}/"
-    cp "${pwd}/tasks/task01.md" "${student_dir}/"
-    cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
+    cp "${pwd}/tasks/task02.md" "${student_dir}/"
+    # cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
     # cp "${pwd}/tasks/*.xls" "${student_dir}/"
     # cp "${pwd}/tasks/task09_5.md" "${student_dir}/task09.md"
     # cp "${pwd}/tasks/specification_01.md" "${student_dir}/specification.md"
