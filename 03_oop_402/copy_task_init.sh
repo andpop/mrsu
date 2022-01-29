fca4cf186f5b8e52e2f35ba99da477263d34c3c5
Andrey Popov <andpop@mail.ru>
2022-01-29 07:39:20 +0300

    Change task1 in oop

--- 03_oop_402/copy_task_init.sh
+++ 03_oop_402/copy_task_init.sh
@@ -7,7 +7,7 @@ echo $pwd
 
 task_number=$1
 
-for student in $(cat "${pwd}/students-list.txt"); do
+for student in $(cat "${pwd}/students_2.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
     student_dir="${BASE_DIR}/402_${student}"
@@ -16,14 +16,14 @@ for student in $(cat "${pwd}/students-list.txt"); do
     git fetch teacher master
     git merge teacher/master -m "Merging master from teacher repo"
 
-    # cp "${pwd}/tasks/README.md" "${student_dir}/"
-    # cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
-    # cp "${pwd}/tasks/PHP_instruction.md" "${student_dir}/"
-    cp "${pwd}/tasks/task07.md" "${student_dir}/task07.md"
+    cp "${pwd}/tasks/README.md" "${student_dir}/"
+    cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
+    cp "${pwd}/tasks/PHP_instruction.md" "${student_dir}/"
+    cp "${pwd}/tasks/task01_2.md" "${student_dir}/task01.md"
     # cp -R "${pwd}/tasks/PHPUnit_example" "${student_dir}/"
 
     git add -A
-    git commit -m "Add task07"
+    git commit -m "Add task01"
     git push teacher master
     cd "${pwd}"
 done
