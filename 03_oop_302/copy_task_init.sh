8a665e97fe386ad42ac49a417c321659fdff180e
Andrey Popov <andpop@mail.ru>
2022-02-05 21:24:36 +0300

    Change task_init in OOP_302

--- 03_oop_302/copy_task_init.sh
+++ 03_oop_302/copy_task_init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BASE_DIR="/home/andrey/labs_402"
+BASE_DIR="/home/andrey/labs_302"
 
 pwd=$PWD
 echo $pwd
@@ -10,7 +10,7 @@ task_number=$1
 for student in $(cat "${pwd}/students-list.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
-    student_dir="${BASE_DIR}/402_${student}"
+    student_dir="${BASE_DIR}/302_${student}"
 
     cd "${student_dir}"
     git fetch teacher master
@@ -19,11 +19,11 @@ for student in $(cat "${pwd}/students-list.txt"); do
     # cp "${pwd}/tasks/README.md" "${student_dir}/"
     # cp "${pwd}/tasks/Git_instruction.md" "${student_dir}/"
     # cp "${pwd}/tasks/PHP_instruction.md" "${student_dir}/"
-    cp "${pwd}/tasks/task07.md" "${student_dir}/task07.md"
+    cp "${pwd}/tasks/task01.md" "${student_dir}/task01.md"
     # cp -R "${pwd}/tasks/PHPUnit_example" "${student_dir}/"
 
     git add -A
-    git commit -m "Add task07"
+    git commit -m "Add task01"
     git push teacher master
     cd "${pwd}"
 done
