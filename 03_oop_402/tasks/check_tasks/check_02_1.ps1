e19ceca5220c6fb5103cbe600e6e95b31126a334
Andrey Popov <andpop@mail.ru>
2022-02-14 23:53:53 +0300

    Add check tasks in oop

---
+++ 03_oop_402/tasks/check_tasks/check_02_1.ps1
@@ -0,0 +1,19 @@
+#!pwsh
+cd Task02_1
+"=================================================="
+pwd
+"=================================================="
+composer install
+phpcs --standard=PSR12 ./src/*
+./vendor/bin/phpunit tests --colors
+
+cd ../Task02_2
+"=================================================="
+pwd
+"=================================================="
+composer install
+phpcs --standard=PSR12 ./src/*
+./vendor/bin/phpunit tests --colors
+
+cd ..
+git push teacher master
