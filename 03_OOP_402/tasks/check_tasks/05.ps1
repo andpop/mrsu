a3c818b5a4d9390eb4a12f704dee8f525801d62f
Andrey Popov <andpop@mail.ru>
2022-03-26 07:37:41 +0300

    Change tasks in OOP

---
+++ 03_OOP_402/tasks/check_tasks/05.ps1
@@ -0,0 +1,28 @@
+#!pwsh
+cd Task05/Task05_1
+"=================================================="
+pwd
+"=================================================="
+composer install
+phpcs --standard=PSR12 ./src/*
+./vendor/bin/phpunit tests --colors
+
+
+cd ../Task05_2
+"=================================================="
+pwd
+"=================================================="
+composer install
+phpcs --standard=PSR12 ./src/*
+./vendor/bin/phpunit tests --colors
+
+cd ../Task05_3
+"=================================================="
+pwd
+"=================================================="
+composer install
+phpcs --standard=PSR12 ./src/*
+./vendor/bin/phpunit tests --colors
+
+cd ..
+git push teacher master
