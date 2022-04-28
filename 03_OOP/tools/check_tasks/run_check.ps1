b6d36792cd2f1651e46c11be74139621b1f8616d
Andrey Popov <andpop@mail.ru>
2022-04-28 21:29:54 +0300

    Change task07

--- 03_OOP/tools/check_tasks/run_check.ps1
+++ 03_OOP/tools/check_tasks/run_check.ps1
@@ -3,7 +3,7 @@
 $studentDirsPath = "/home/andrey/labs_302/"
 $teacherRepo = "https://api.github.com/repos/andpop-mrsu/" 
 $headers = @{}
-$token = ""
+$token = "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"
 $headers.Add("Authorization", "Token $token")
 
 . ./pull_requests.ps1
