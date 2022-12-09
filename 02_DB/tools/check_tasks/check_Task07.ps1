2efe09892d2a630e16793986198bee50c57f0b59
Andrey Popov <andpop@mail.ru>
2022-12-09 19:04:51 +0300

    Add task check in DB

---
+++ 02_DB/tools/check_tasks/check_Task07.ps1
@@ -0,0 +1,34 @@
+param(
+    [string]$studentDir,
+    [string]$task
+)
+
+function Check-Task {
+    Write-Host "--------------------------------------------------" -ForegroundColor blue
+    Write-Host "Task: ", (Get-Location).Path -ForegroundColor blue
+
+    $doneChecks = $failureChecks = 0
+
+    if (Check-Path -Path task7_cli.php) { $doneChecks++ } else { $failureChecks++ }
+    if (Check-Path -Path index.php) { $doneChecks++ } else { $failureChecks++ }
+
+    Write-DelimiterString
+    Write-Host "Total ok: $doneChecks" -ForegroundColor green
+    Write-Host "Total error: $failureChecks" -ForegroundColor red
+}
+
+. ./checks_for_task.ps1
+
+$taskDir = "$studentDir/$task"
+
+if (-not (Test-Path $taskDir)) {
+    Write-Host "There are no folder $taskDir" -ForegroundColor red
+    return
+}
+
+Push-Location "$taskDir"
+Check-Task
+Pop-Location
+    
+Write-Host "--------------------------------------------------" -ForegroundColor blue
+
