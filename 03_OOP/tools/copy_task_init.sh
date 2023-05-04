0af8e8b36864aa842aa0520bbcb720e699ecdf52
Andrey Popov <andpop@mail.ru>
2023-05-04 22:08:23 +0300

    Change Task06 in OOP

--- 03_OOP/tools/copy_task_init.sh
+++ 03_OOP/tools/copy_task_init.sh
@@ -20,12 +20,15 @@ for student in $(cat "${pwd}/students_2.txt"); do
     # cp "${pwd}/../tasks/Git_instruction.md" "${student_dir}/"
     # cp "${pwd}/../tasks/PHP_instruction.md" "${student_dir}/"
     # cp "${pwd}/../tasks/Pull_Request_flow.pdf" "${student_dir}/"
-    cp "${pwd}/../tasks/task01.md" "${student_dir}/task01.md"
-    cp "${pwd}/../tasks/task03_2.md" "${student_dir}/task03.md"
+    # cp "${pwd}/../tasks/task01.md" "${student_dir}/task01.md"
+    # cp "${pwd}/../tasks/task06_1.md" "${student_dir}/task06.md"
+    mkdir -p "${student_dir}/Task06/"
+    cp -R "${pwd}/../tasks/Task06_2" "${student_dir}/Task06/Task06_1"
+    # rm -rf "${student_dir}/Task06"
     # cp -R "${pwd}/../tasks/PHPUnit_example" "${student_dir}/"
 
     git add -A
-    git commit -m "Add task03"
+    git commit -m "Add task06"
     git push teacher master
     cd "${pwd}"
 done
