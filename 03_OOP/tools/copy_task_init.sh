ac46757786646eca2526e0ce677ef1a7251b089e
Andrey Popov <andpop@mail.ru>
2023-05-17 16:15:43 +0300

    Add Task07 checking in OOP

--- 03_OOP/tools/copy_task_init.sh
+++ 03_OOP/tools/copy_task_init.sh
@@ -7,7 +7,7 @@ echo $pwd
 
 task_number=$1
 
-for student in $(cat "${pwd}/students_2.txt"); do
+for student in $(cat "${pwd}/students-list.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
     student_dir="${BASE_DIR}/${student}"
@@ -21,14 +21,14 @@ for student in $(cat "${pwd}/students_2.txt"); do
     # cp "${pwd}/../tasks/PHP_instruction.md" "${student_dir}/"
     # cp "${pwd}/../tasks/Pull_Request_flow.pdf" "${student_dir}/"
     # cp "${pwd}/../tasks/task01.md" "${student_dir}/task01.md"
-    # cp "${pwd}/../tasks/task06_1.md" "${student_dir}/task06.md"
-    mkdir -p "${student_dir}/Task06/"
-    cp -R "${pwd}/../tasks/Task06_2" "${student_dir}/Task06/Task06_1"
+    cp "${pwd}/../tasks/task08.md" "${student_dir}/task08.md"
+    # mkdir -p "${student_dir}/Task06/"
+    # cp -R "${pwd}/../tasks/Task06_2" "${student_dir}/Task06/Task06_1"
     # rm -rf "${student_dir}/Task06"
     # cp -R "${pwd}/../tasks/PHPUnit_example" "${student_dir}/"
 
     git add -A
-    git commit -m "Add task06"
+    git commit -m "Add task08"
     git push teacher master
     cd "${pwd}"
 done
