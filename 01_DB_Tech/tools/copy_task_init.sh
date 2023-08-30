d8fff304c37eb5ed8254a405e91f022717f68f17
Andrey Popov <andpop@mail.ru>
2023-08-30 10:00:38 +0300

    Add task01

--- 01_DB_Tech/tools/copy_task_init.sh
+++ 01_DB_Tech/tools/copy_task_init.sh
@@ -7,25 +7,25 @@ echo $pwd
 
 task_number=$2
 
-for student in $(cat "${pwd}/students-list.txt"); do
-# for student in $(cat "${pwd}/list5.txt"); do
+# for student in $(cat "${pwd}/students-list.txt"); do
+for student in $(cat "${pwd}/option_1.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
-    student_dir="${BASE_DIR}/402_${student}"
+    student_dir="${BASE_DIR}/402_DBTech_${student}"
 
     cd "${student_dir}"
     git fetch teacher master
     git merge teacher/master -m "Merging master from teacher repo"
 
-    # cp "${pwd}/tasks/README.md" "${student_dir}/"
-    # cp "${pwd}/tasks/task01.md" "${student_dir}/"
-    # cp "${pwd}/../Git_instruction.md" "${student_dir}/"
+    cp "${pwd}/tasks/README.md" "${student_dir}/"
+    cp "${pwd}/tasks/task01.md" "${student_dir}/"
+    cp "${pwd}/../Git_instruction.md" "${student_dir}/"
     # cp "${pwd}/tasks/*.xls" "${student_dir}/"
-    cp "${pwd}/../tasks/task07.md" "${student_dir}/task07.md"
-    # cp "${pwd}/tasks/specification_01.md" "${student_dir}/specification.md"
+    # cp "${pwd}/../tasks/task01.md" "${student_dir}/task01.md"
+    cp "${pwd}/tasks/general_specification_01.md" "${student_dir}/general_specification.md"
 
     git add -A
-    git commit -m "Add task07"
+    git commit -m "Add task01"
     git push teacher master
     cd "${pwd}"
 done
