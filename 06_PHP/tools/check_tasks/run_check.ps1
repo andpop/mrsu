0b495e9b38d5975af1bf7bda25bf820f3eac04a8
Andrey Popov <andpop@mail.ru>
2025-02-03 15:33:39 +0300

    Change script for check in PHP

---
+++ 06_PHP/tools/check_tasks/run_check.ps1
@@ -0,0 +1,18 @@
+#!pwsh
+
+$studentDirsPath = "/home/andrey/labs/PHP"
+$teacherRepo = "https://api.github.com/repos/andpop-mrsu" 
+$headers = @{}
+$token = ''
+if (Test-Path ./token.txt) { $token = (Get-Content ./token.txt) }
+$headers.Add("Authorization", "Token $token")
+
+. ./pull_requests.ps1
+
+if (Test-Path "./log.txt") { Remove-Item ./log.txt }
+Start-Transcript -Path "log.txt" -UseMinimalHeader
+
+$studentDirs = (Get-ChildItem $studentDirsPath -Attributes Directory) 
+$studentDirs | ForEach-Object {Check-Student -studentName $_.Name}
+
+Stop-Transcript
