66f9bbb21f1b497eaa8e8fc2ff4d9f5c37b26777
Andrey Popov <andpop@mail.ru>
2025-02-21 08:24:50 +0300

    Change task in PHP

--- 06_PHP/tools/copy_task_init.sh
+++ 06_PHP/tools/copy_task_init.sh
@@ -7,8 +7,8 @@ echo $pwd
 
 task_number=$2
 
-# for student in $(cat "${pwd}/reps.txt"); do
-for student in $(cat "${pwd}/group4.txt"); do
+for student in $(cat "${pwd}/reps.txt"); do
+# for student in $(cat "${pwd}/group4.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
     student_dir="${BASE_DIR}/${student}"
@@ -18,14 +18,14 @@ for student in $(cat "${pwd}/group4.txt"); do
     git merge teacher/master -m "Merging master from teacher repo"
 
     # cp "${pwd}/../tasks/README.md" "${student_dir}/"
-    # cp "${pwd}/../tasks/task02.md" "${student_dir}/"
+    cp "${pwd}/../tasks/task03.md" "${student_dir}/"
     # cp "${pwd}/../Git_instruction.md" "${student_dir}/"
     #
-    cp "${pwd}/../tasks/spec_4.md" "${student_dir}/specification.md"
+    # cp "${pwd}/../tasks/spec_4.md" "${student_dir}/specification.md"
 
     git add -A
-    git commit -m "Change game specification"
-    # git commit -m "Add task02"
+    # git commit -m "Change game specification"
+    git commit -m "Add task03"
     git push teacher master
     cd "${pwd}"
 done
