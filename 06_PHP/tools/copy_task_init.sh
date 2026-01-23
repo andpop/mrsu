6d9104be18e0cbb203b91ab3a7f6e57db25c5ed1
Andrey Popov <andpop@mail.ru>
2026-01-23 16:29:26 +0300

    Add task01

--- 06_PHP/tools/copy_task_init.sh
+++ 06_PHP/tools/copy_task_init.sh
@@ -7,8 +7,8 @@ echo $pwd
 
 task_number=$2
 
-for student in $(cat "${pwd}/reps.txt"); do
-# for student in $(cat "${pwd}/group4.txt"); do
+# for student in $(cat "${pwd}/reps.txt"); do
+for student in $(cat "${pwd}/group1.txt"); do
     echo "----------------------------------------------------------------------------------"
     echo $student
     student_dir="${BASE_DIR}/${student}"
@@ -17,15 +17,15 @@ for student in $(cat "${pwd}/reps.txt"); do
     git fetch teacher master
     git merge teacher/master -m "Merging master from teacher repo"
 
-    # cp "${pwd}/../tasks/README.md" "${student_dir}/"
-    cp "${pwd}/../tasks/task04.md" "${student_dir}/"
-    # cp "${pwd}/../Git_instruction.md" "${student_dir}/"
+    cp "${pwd}/../tasks/README.md" "${student_dir}/"
+    cp "${pwd}/../tasks/task01.md" "${student_dir}/"
+    cp "${pwd}/../Git_instruction.md" "${student_dir}/"
     #
-    # cp "${pwd}/../tasks/spec_4.md" "${student_dir}/specification.md"
+    cp "${pwd}/../tasks/spec_1.md" "${student_dir}/specification.md"
 
     git add -A
     # git commit -m "Change game specification"
-    git commit -m "Add task04"
+    git commit -m "Add task01"
     git push teacher master
     cd "${pwd}"
 done
