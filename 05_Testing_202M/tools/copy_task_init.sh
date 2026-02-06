94f6a761e22ea26283176fd6d2d101aac070b15f
Andrey Popov <andpop@mail.ru>
2026-02-06 09:10:25 +0300

    Change tasks in testing

--- 05_Testing_202M/tools/copy_task_init.sh
+++ 05_Testing_202M/tools/copy_task_init.sh
@@ -16,16 +16,16 @@ for student in $(cat "${pwd}/students-list_202M.txt"); do
     git fetch teacher master
     git merge teacher/master -m "Merging master from teacher repo"
 
-    cp "${pwd}/../tasks/README.md" "${student_dir}/"
-    cp "${pwd}/../tasks/Git_instruction.md" "${student_dir}/"
+    # cp "${pwd}/../tasks/README.md" "${student_dir}/"
+    # cp "${pwd}/../tasks/Git_instruction.md" "${student_dir}/"
     cp "${pwd}/../tasks/task01.md" "${student_dir}/task01.md"
     cp "${pwd}/../tasks/task02.md" "${student_dir}/task02.md"
     cp "${pwd}/../tasks/task03.md" "${student_dir}/task03.md"
-    cp "${pwd}/../tasks/report_example_1.pdf" "${student_dir}/"
-    cp "${pwd}/../tasks/report_example_2.pdf" "${student_dir}/"
+    # cp "${pwd}/../tasks/report_example_1.pdf" "${student_dir}/"
+    # cp "${pwd}/../tasks/report_example_2.pdf" "${student_dir}/"
 
     git add -A
-    git commit -m "Add tasks"
+    git commit -m "Change tasks"
     git push teacher master
     cd "${pwd}"
 done
