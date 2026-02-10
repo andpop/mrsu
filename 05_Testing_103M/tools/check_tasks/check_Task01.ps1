ca83d9fbde51f547f1bd9b7873c81dfdc85672f4
Andrey Popov <andpop@mail.ru>
2026-02-10 16:29:57 +0300

    Change checking scripts in testing_103M

---
+++ 05_Testing_103M/tools/check_tasks/check_Task01.ps1
@@ -0,0 +1,34 @@
+param(
+    [string]$studentDir,
+    [string]$task
+)
+
+function Check-Task {
+    Write-Host "--------------------------------------------------" -ForegroundColor blue
+    Write-Host "Task: ", (Get-Location).Path -ForegroundColor blue
+
+    $doneChecks = $failureChecks = 0
+
+    if (Check-Path -Path questions.docx) { $doneChecks++ } else { $failureChecks++ }
+    if (Check-Path -Path spec.docx) { $doneChecks++ } else { $failureChecks++ }
+
+    Write-DelimiterString
+    Write-Host "Total ok: $doneChecks" -ForegroundColor green
+    Write-Host "Total error: $failureChecks" -ForegroundColor red
+}
+
+
+. ./checks_for_task.ps1
+
+$taskDir = "$studentDir/$task"
+
+if (-not (Test-Path $taskDir)) {
+    Write-Host "There are no folder $taskDir" -ForegroundColor red
+    return
+}
+
+Push-Location "$taskDir"
+Check-Task
+Pop-Location
+    
+Write-Host "--------------------------------------------------" -ForegroundColor blue
